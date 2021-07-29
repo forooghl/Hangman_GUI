9213b8d82901cea874cc4cd8b65a822258f6cab9
Foroogh_l <foroogh.msl@gmail.com>
2021-07-29 14:12:01 +0430

    fix button function

--- Hangman.py
+++ Hangman.py
@@ -81,7 +81,7 @@ def hangman():
 
 def KillHangman():
     global wrong
-    
+
     draw.pencolor("white")
 
     if wrong == 1:
@@ -183,107 +183,107 @@ answerLable.place(x = 400 - answerLength*2 , y = 70)
 answerLable['bg'] = 'white'
 
 #Alphabet Keyboard
-Q = Button(main , width = 4 , text = "Q" , command = lambda key = 'Q' : Keyword(key))
+Q = Button(main , width = 4 , text = "Q" , command = lambda key = 'q' : Keyword(key))
 Q.pack()
 Q.place(x = 225 , y = 250)
 
-W = Button(main , width = 4 , text = "W" , command = lambda key = 'W' : Keyword(key))
+W = Button(main , width = 4 , text = "W" , command = lambda key = 'w' : Keyword(key))
 W.pack()
 W.place(x = 270 , y = 250)
 
-E = Button(main , width = 4 , text = "E" , command = lambda key = 'E' : Keyword(key))
+E = Button(main , width = 4 , text = "E" , command = lambda key = 'e' : Keyword(key))
 E.pack()
 E.place(x = 315 , y = 250)
 
-R = Button(main , width = 4 , text = "R" , command = lambda key = 'R' : Keyword(key))
+R = Button(main , width = 4 , text = "R" , command = lambda key = 'r' : Keyword(key))
 R.pack() 
 R.place(x = 360 , y = 250)
 
-T = Button(main , width = 4 , text = "T" , command = lambda key = 'T' : Keyword(key))
+T = Button(main , width = 4 , text = "T" , command = lambda key = 't' : Keyword(key))
 T.pack()
 T.place(x = 405 , y = 250)
 
-Y = Button(main , width = 4 , text = "Y" , command = lambda key = 'Y' : Keyword(key))
+Y = Button(main , width = 4 , text = "Y" , command = lambda key = 'y' : Keyword(key))
 Y.pack()
 Y.place(x = 450 , y = 250)
 
-U = Button(main , width = 4 , text = "U" , command = lambda key = 'U' : Keyword(key))
+U = Button(main , width = 4 , text = "U" , command = lambda key = 'u' : Keyword(key))
 U.pack()
 U.place(x = 495 , y = 250)
 
-I = Button(main , width = 4 , text = "I" , command = lambda key = 'I' : Keyword(key))
+I = Button(main , width = 4 , text = "I" , command = lambda key = 'i' : Keyword(key))
 I.pack() 
 I.place(x = 540 , y = 250)
 
-O = Button(main , width = 4 , text = "O" , command = lambda key = 'O' : Keyword(key))
+O = Button(main , width = 4 , text = "O" , command = lambda key = 'o' : Keyword(key))
 O.pack()
 O.place(x = 585 , y = 250)
 
-P = Button(main , width = 4 , text = "P" , command = lambda key = 'P' : Keyword(key))
+P = Button(main , width = 4 , text = "P" , command = lambda key = 'p' : Keyword(key))
 P.pack()
 P.place(x = 630 , y = 250)
 
-A = Button(main , width = 4 , text = "A" , command = lambda key = 'A' : Keyword(key))
+A = Button(main , width = 4 , text = "A" , command = lambda key = 'a' : Keyword(key))
 A.pack() 
 A.place(x = 240 , y = 280)
 
-S = Button(main , width = 4 , text = "S" , command = lambda key = 'S' : Keyword(key))
+S = Button(main , width = 4 , text = "S" , command = lambda key = 's' : Keyword(key))
 S.pack() 
 S.place(x = 285 , y = 280)
 
-D = Button(main , width = 4 , text = "D" , command = lambda key = 'D' : Keyword(key))
+D = Button(main , width = 4 , text = "D" , command = lambda key = 'd' : Keyword(key))
 D.pack() 
 D.place(x = 330 , y = 280)
 
-F = Button(main , width = 4 , text = "F" , command = lambda key = 'F' : Keyword(key))
+F = Button(main , width = 4 , text = "F" , command = lambda key = 'f' : Keyword(key))
 F.pack() 
 F.place(x = 375 , y = 280)
 
-G = Button(main , width = 4 , text = "G" , command = lambda key = 'G' : Keyword(key))
+G = Button(main , width = 4 , text = "G" , command = lambda key = 'g' : Keyword(key))
 G.pack() 
 G.place(x = 420 , y = 280)
 
-H = Button(main , width = 4 , text = "H" , command = lambda key = 'H' : Keyword(key))
+H = Button(main , width = 4 , text = "H" , command = lambda key = 'h' : Keyword(key))
 H.pack() 
 H.place(x = 465 , y = 280)
 
-J = Button(main , width = 4 , text = "J" , command = lambda key = 'J' : Keyword(key))
+J = Button(main , width = 4 , text = "J" , command = lambda key = 'j' : Keyword(key))
 J.pack() 
 J.place(x = 510 , y = 280)
 
-K = Button(main , width = 4 , text = "K" , command = lambda key = 'K' : Keyword(key))
+K = Button(main , width = 4 , text = "K" , command = lambda key = 'k' : Keyword(key))
 K.pack()  
 K.place(x = 555 , y = 280)
 
-L = Button(main , width = 4 , text = "L" , command = lambda key = 'L' : Keyword(key))
+L = Button(main , width = 4 , text = "L" , command = lambda key = 'l' : Keyword(key))
 L.pack() 
 L.place(x = 600 , y = 280)
 
-Z = Button(main , width = 4 , text = "Z" , command = lambda key = 'Z' : Keyword(key))
+Z = Button(main , width = 4 , text = "Z" , command = lambda key = 'z' : Keyword(key))
 Z.pack()  
 Z.place(x = 255 , y = 310)
 
-X = Button(main , width = 4 , text = "X" , command = lambda key = 'X' : Keyword(key))
+X = Button(main , width = 4 , text = "X" , command = lambda key = 'x' : Keyword(key))
 X.pack() 
 X.place(x = 300 , y = 310)
 
-C = Button(main , width = 4 , text = "C" , command = lambda key = 'C' : Keyword(key))
+C = Button(main , width = 4 , text = "C" , command = lambda key = 'c' : Keyword(key))
 C.pack() 
 C.place(x = 345 , y = 310)
 
-V = Button(main , width = 4 , text = "V" , command = lambda key = 'V' : Keyword(key))
+V = Button(main , width = 4 , text = "V" , command = lambda key = 'v' : Keyword(key))
 V.pack() 
 V.place(x = 390 , y = 310)
 
-B = Button(main , width = 4 , text = "B" , command = lambda key = 'B' : Keyword(key))
+B = Button(main , width = 4 , text = "B" , command = lambda key = 'b' : Keyword(key))
 B.pack() 
 B.place(x = 435 , y = 310)
 
-N = Button(main , width = 4 , text = "N" , command = lambda key = 'N' : Keyword(key))
+N = Button(main , width = 4 , text = "N" , command = lambda key = 'n' : Keyword(key))
 N.pack() 
 N.place(x = 480 , y = 310)
 
-M = Button(main , width = 4 , text = "M" , command = lambda key = 'M' : Keyword(key))
+M = Button(main , width = 4 , text = "M" , command = lambda key = 'm' : Keyword(key))
 M.pack()
 M.place(x = 525 , y = 310)
 
