8e499e523773900d80229c62cc8cde4d3b66c5bf
Foroogh_l <foroogh.msl@gmail.com>
2021-07-31 02:01:58 +0430

    added numkey

--- Hangman.py
+++ Hangman.py
@@ -393,6 +393,57 @@ answerLable.place(x = 290 , y = 70)
 answerLable['bg'] = '#FFCD73'
 lastAnswer = answerLable["text"]
 
+#numpad
+zero = tkinter.Button(main , text = "0" , width = 4 , command = lambda num = "0" : Keyword(num))
+zero.pack()
+zero.place(x = 225 , y = 220)
+zero['bg'] = '#FF9200'
+
+one = tkinter.Button(main , text = "1" , width = 4 , command = lambda num = "1" :  Keyword(num))
+one.pack()
+one.place(x = 270 , y = 220)
+one['bg'] = '#FF9200'
+
+two = tkinter.Button(main , text = "2" , width = 4 , command = lambda num = "2" :  Keyword(num))
+two.pack()
+two.place(x = 315 , y = 220)
+two['bg'] = '#FF9200'
+
+three = tkinter.Button(main , text = "3" , width = 4 , command = lambda num = "3" :  Keyword(num))
+three.pack()
+three.place(x = 360 , y = 220)
+three['bg'] = '#FF9200'
+
+four = tkinter.Button(main , text = "4" , width = 4 , command = lambda num = "4" :  Keyword(num))
+four.pack()
+four.place(x = 405 , y = 220)
+four['bg'] = '#FF9200'
+
+five = tkinter.Button(main , text = "5" , width = 4 , command = lambda num = "5" :  Keyword(num))
+five.pack()
+five.place(x = 450 , y = 220)
+five['bg'] = '#FF9200'
+
+six = tkinter.Button(main , text = "6" , width = 4 , command = lambda num = "6" :  Keyword(num))
+six.pack()
+six.place(x = 495 , y = 220)
+six['bg'] = '#FF9200'
+
+seven = tkinter.Button(main , text = "7" , width = 4 , command = lambda num = "7" :  Keyword(num))
+seven.pack()
+seven.place(x = 540 , y = 220)
+seven['bg'] = '#FF9200'
+
+eight = tkinter.Button(main , text = "8" , width = 4 , command = lambda num = "8" :  Keyword(num))
+eight.pack()
+eight.place(x = 585 , y = 220)
+eight['bg'] = '#FF9200'
+
+nine = tkinter.Button(main , text = "9" , width = 4 , command = lambda num = "9" :  Keyword(num))
+nine.pack()
+nine.place(x = 630 , y = 220)
+nine['bg'] = '#FF9200'
+
 #Alphabet Keyboard
 Q = Button(main , width = 4 , text = "Q" , command = lambda key = 'q' : Keyword(key))
 Q.pack()
@@ -446,82 +497,82 @@ P['bg'] = "#FF9200"
 
 A = Button(main , width = 4 , text = "A" , command = lambda key = 'a' : Keyword(key))
 A.pack() 
-A.place(x = 240 , y = 280)
+A.place(x = 225 , y = 280)
 A['bg'] = "#FF9200"
 
 S = Button(main , width = 4 , text = "S" , command = lambda key = 's' : Keyword(key))
 S.pack() 
-S.place(x = 285 , y = 280)
+S.place(x = 270 , y = 280)
 S['bg'] = "#FF9200"
 
 D = Button(main , width = 4 , text = "D" , command = lambda key = 'd' : Keyword(key))
 D.pack() 
-D.place(x = 330 , y = 280)
+D.place(x = 315 , y = 280)
 D['bg'] = "#FF9200"
 
 F = Button(main , width = 4 , text = "F" , command = lambda key = 'f' : Keyword(key))
 F.pack() 
-F.place(x = 375 , y = 280)
+F.place(x = 360 , y = 280)
 F['bg'] = "#FF9200"
 
 G = Button(main , width = 4 , text = "G" , command = lambda key = 'g' : Keyword(key))
 G.pack() 
-G.place(x = 420 , y = 280)
+G.place(x = 405 , y = 280)
 G['bg'] = "#FF9200"
 
 H = Button(main , width = 4 , text = "H" , command = lambda key = 'h' : Keyword(key))
 H.pack() 
-H.place(x = 465 , y = 280)
+H.place(x = 450 , y = 280)
 H['bg'] = "#FF9200"
 
 J = Button(main , width = 4 , text = "J" , command = lambda key = 'j' : Keyword(key))
 J.pack() 
-J.place(x = 510 , y = 280)
+J.place(x = 495 , y = 280)
 J['bg'] = "#FF9200"
 
 K = Button(main , width = 4 , text = "K" , command = lambda key = 'k' : Keyword(key))
 K.pack()  
-K.place(x = 555 , y = 280)
+K.place(x = 540 , y = 280)
 K['bg'] = "#FF9200"
 
 L = Button(main , width = 4 , text = "L" , command = lambda key = 'l' : Keyword(key))
 L.pack() 
-L.place(x = 600 , y = 280)
+L.place(x = 585 , y = 280)
 L['bg'] = "#FF9200"
 
 Z = Button(main , width = 4 , text = "Z" , command = lambda key = 'z' : Keyword(key))
 Z.pack()  
-Z.place(x = 255 , y = 310)
+Z.place(x = 630 , y = 280)
 Z['bg'] = "#FF9200"
 
 X = Button(main , width = 4 , text = "X" , command = lambda key = 'x' : Keyword(key))
 X.pack() 
-X.place(x = 300 , y = 310)
+X.place(x = 315 , y = 310)
 X['bg'] = "#FF9200"
 
 C = Button(main , width = 4 , text = "C" , command = lambda key = 'c' : Keyword(key))
 C.pack() 
-C.place(x = 345 , y = 310)
+C.place(x = 360 , y = 310)
 C['bg'] = "#FF9200"
 
 V = Button(main , width = 4 , text = "V" , command = lambda key = 'v' : Keyword(key))
 V.pack() 
-V.place(x = 390 , y = 310)
+V.place(x = 405 , y = 310)
 V['bg'] = "#FF9200"
 
 B = Button(main , width = 4 , text = "B" , command = lambda key = 'b' : Keyword(key))
 B.pack() 
-B.place(x = 435 , y = 310)
+B.place(x = 450 , y = 310)
 B['bg'] = "#FF9200"
 
 N = Button(main , width = 4 , text = "N" , command = lambda key = 'n' : Keyword(key))
 N.pack() 
-N.place(x = 480 , y = 310)
+N.place(x = 495 , y = 310)
 N['bg'] = "#FF9200"
 
 M = Button(main , width = 4 , text = "M" , command = lambda key = 'm' : Keyword(key))
 M.pack()
-M.place(x = 525 , y = 310)
+M.place(x = 540 , y = 310)
 M['bg'] = "#FF9200"
 
 #play again button
